73efbf59d26dc456813be92d562cd5801b0b6247
Salmane Tamo <tamosalmane@gmail.com>
2021-02-21 00:50:38 +0400

    Added styling for BottomNavigationBar

--- lib/home/ui/home_screen.dart
+++ lib/home/ui/home_screen.dart
@@ -66,20 +66,56 @@ class _HomeScreenState extends State<HomeScreen>
           }).toList(),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: destinations.map((HomeScreenDestination destination) {
-          return BottomNavigationBarItem(
-            icon: Icon(destination.icon),
-            backgroundColor: Colors.grey,
-            label: destination.title,
-          );
-        }).toList(),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(24),
+            topLeft: Radius.circular(24),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              spreadRadius: 0,
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(24.0),
+            topRight: Radius.circular(24.0),
+          ),
+          child: Container(
+            color: Colors.white,
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                color: Colors.grey[200],
+              ),
+              padding: EdgeInsets.all(8.0),
+              child: BottomNavigationBar(
+                elevation: 0.0,
+                selectedItemColor: Colors.black,
+                unselectedItemColor: Colors.grey,
+                currentIndex: _currentIndex,
+                showUnselectedLabels: true,
+                onTap: (int index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                items: destinations.map((HomeScreenDestination destination) {
+                  return BottomNavigationBarItem(
+                    icon: Icon(destination.icon),
+                    label: destination.title,
+                    backgroundColor: Colors.grey[200],
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
